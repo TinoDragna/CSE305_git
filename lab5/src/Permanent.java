68458867021d9cedafbebb9a824a247a897f61b0
Phạm Nguyễn Bảo Trang <167573896+TinoDragna@users.noreply.github.com>
2024-11-12 09:04:19 +0700

    9:04 bilder pattern lab 5 cse 305

---
+++ lab5/src/Permanent.java
@@ -0,0 +1,44 @@
+public class Permanent implements Contract{
+
+    private int contractID;
+    private int propertyID;
+    private int tenantID;
+    private double rentAmount;
+
+    @Override
+    public Contract buildContractID(int contractID) {
+        this.contractID = contractID;
+        return this;
+    }
+
+    @Override
+    public Contract buildPropertyID(int propertyID) {
+        this.propertyID = propertyID;
+        return this;
+    }
+
+    @Override
+    public Contract buildTenantID(int tenantID) {
+        this.tenantID = tenantID;
+        return this;
+    }
+
+    @Override
+    public Contract buildRentAmount(double rentAmount) {
+        this.rentAmount = rentAmount;
+        return this;
+    }
+
+    @Override
+    public Contract signContract() {
+        System.out.println("Permanent contract signed with details: " + this);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Permanent{contractID=" + contractID + ", propertyID=" + propertyID +
+                ", tenantID=" + tenantID + ", rentAmount=" + rentAmount + "}";
+    }
+
+}
