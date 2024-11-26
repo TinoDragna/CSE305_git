79901becf6dd33c9c71fa90e0820ce7de0b2a003
nguyenvuphuongtrang <trang.nguyenvu.cit21@eiu.edu.vn>
2024-11-26 09:31:28 +0700

    lab6

---
+++ Lab/HiPriorityConcreteCreator.java
@@ -0,0 +1,8 @@
+public class HiPriorityConcreteCreator extends RequestCreator{
+
+    @Override
+    public RequestProduct createRequest() {
+        return new HighPriorityConcrete();
+    }
+    
+}
---
+++ Lab/MidPriorityConcreteCreator.java
@@ -0,0 +1,8 @@
+public class MidPriorityConcreteCreator extends RequestCreator{
+
+    @Override
+    public RequestProduct createRequest() {
+        return new MidPriorityConcrete();
+    }
+    
+}
